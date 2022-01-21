a562317c94cf86ed3c1ff23c75db3828c50ee24a
AmirVafaei <amirvafaei.contact@gmail.com>
2022-01-21 17:04:18 +0100

    Add changes in RandomColor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import RandomColor from './RandomColor';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RandomColor />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
