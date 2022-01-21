b4a000729c6401eb872f3efb58a388c97e2591e8
AmirVafaei <amirvafaei.contact@gmail.com>
2022-01-21 15:32:20 +0100

    Add changes in to App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import React, { useState } from 'react';
 const App = () => {
   const [hex, setHex] = useState('#ffffff');
   const randomizedHex = () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16000000).toString(16);
+    const randomColor =
+      '#' +
+      Math.floor(Math.random() * 16000000)
+        .toString(16)
+        .slice(2, 8);
 
     setHex(randomColor);
   };
@@ -16,7 +20,6 @@ const App = () => {
         backgroundColor: `${hex}`,
         minHeight: '33.34vh',
         overflow: 'hidden',
-        zoom: 3,
       }}
     >
       <div style={{ backgroundColor: `${hex}` }}>Generated Color: {hex}</div>
